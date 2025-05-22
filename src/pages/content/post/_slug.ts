093e919e6427e7af4c0ad966886548295b1e49f8
kennycallado <kennycallado@hotmail.com>
2025-05-22 21:54:02 +0200

    fix(pages)
    content: slug: bun fails because of browser primitives

--- src/pages/content/post/_slug.ts
+++ src/pages/content/post/_slug.ts
@@ -1,6 +1,5 @@
 import { html } from "lit";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
-import { type WlDatabase } from "$lib/client/webslab/database.ts";
 
 // deno-fmt-ignore-start
 export const template = (post: any) =>
@@ -9,17 +8,3 @@ export const template = (post: any) =>
     <section>${post.content.map((content:string) => unsafeHTML(content))}</section>
 `;
 // deno-fmt-ignore-end
-
-export const hydrate = () => {
-  const url = new URL(location.href);
-  const slug = url.searchParams.get("slug") ||
-    url.pathname.split("/").slice(-2)[0];
-
-  const wlDatabase = document.querySelector("wl-database") as WlDatabase;
-  wlDatabase.query = `SELECT * FROM ONLY post WHERE slug = '${slug}'`;
-  wlDatabase.template = template;
-
-  wlDatabase.addEventListener("wl-task:completed", () => {
-    console.log("Database is ready");
-  });
-}
