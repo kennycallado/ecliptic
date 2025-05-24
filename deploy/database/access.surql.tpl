1919479604305be2ee01d6bb1d85f13ace505d9b
kennycallado <kennycallado@hotmail.com>
2025-05-24 21:49:56 +0200

    feat(pacakge)
    initial admin user:
    - custom functions for hash and verify
    - env for admin credentials
    - access migration template
    - kube job with sealed credentials

--- deploy/database/access.surql.tpl
+++ deploy/database/access.surql.tpl
@@ -10,3 +10,22 @@ DEFINE ACCESS IF NOT EXISTS users ON DATABASE TYPE RECORD
   )
   WITH JWT ALGORITHM HS512 KEY '{{SECRET_DB_KEY}}'
   WITH ISSUER KEY '{{SECRET_DB_KEY}}' DURATION FOR TOKEN 7d, FOR SESSION NONE;
+
+LET $user = UPSERT user MERGE {
+  banned: false,
+  createdAt: time::now(),
+  email: '{{SECRET_ADMIN_EMAIL}}',
+  emailVerified: true,
+  name: 'Admin',
+  role: 'admin',
+  updatedAt: time::now(),
+} WHERE email = '{{SECRET_ADMIN_EMAIL}}';
+
+UPSERT account MERGE {
+  accountId: $user[0].id,
+  createdAt: time::now(),
+  password: crypto::argon2::generate('{{SECRET_ADMIN_PASSWORD}}'),
+  providerId: 'credential',
+  updatedAt: time::now(),
+  userId: $user[0].id,
+} WHERE userId = $user[0].id;
--- src/lib/server/services/auth.ts
+++ src/lib/server/services/auth.ts
@@ -6,6 +6,7 @@ import { surrealAdapter } from "surreal-better-auth";
 
 import { AUTH_SECRET, DB_SERVER } from "$lib/server/consts.ts";
 import { adminRole, theraRoles, userRole } from "$lib/roles.ts";
+import { catchErrorTyped } from "$lib/utils";
 
 // Initialize server-side DB connection once
 const serverDb = new Surreal();
@@ -19,7 +20,6 @@ serverDb
 
 export const auth = betterAuth({
   database: surrealAdapter(serverDb),
-  emailAndPassword: { enabled: true },
   secret: AUTH_SECRET,
   trustedOrigins: ["http://localhost:3000"],
   advanced: { database: { generateId: false } },
@@ -31,6 +31,61 @@ export const auth = betterAuth({
     },
     expiresIn: 60 * 60 * 24 * 14, // two weeks // NOTE: should > surrealdb TOKEN
     updateAge: 60 * 60 * 24 * 2, // two days
+  },
+
+  emailAndPassword: {
+    enabled: true,
+    password: {
+      hash: async (password) => {
+        { // scoped: db ready
+          const { error } = await catchErrorTyped(serverDb.ready);
+          if (error) {
+            console.error("SurrealDB is not ready:", error);
+            throw new Error("Database connection error");
+          }
+        }
+
+        const { error, data } = await catchErrorTyped(
+          serverDb.query<string[]>("crypto::argon2::generate($password)", {
+            password,
+          }),
+        );
+
+        if (error) {
+          console.error("Error generating password hash:", error);
+          throw new Error("Password hashing error");
+        }
+
+        return data[0];
+      },
+
+      verify: async ({ hash, password }) => {
+        { // scoped: db ready
+          const { error } = await catchErrorTyped(serverDb.ready);
+          if (error) {
+            console.error("SurrealDB is not ready:", error);
+            throw new Error("Database connection error");
+          }
+        }
+
+        const { error, data } = await catchErrorTyped(
+          serverDb.query<boolean[]>(
+            "crypto::argon2::compare($hash, $password)",
+            {
+              password,
+              hash,
+            },
+          ),
+        );
+
+        if (error) {
+          console.error("Error verifying password hash:", error);
+          throw new Error("Password verification error");
+        }
+
+        return data[0];
+      },
+    },
   },
 
   plugins: [
