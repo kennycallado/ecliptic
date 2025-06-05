182c40971c768dfc9294d109c4eea9e65d4b46bc
kennycallado <kennycallado@hotmail.com>
2025-06-05 10:00:57 +0200

    update(components)
    minnor changes: TestApi, TestDb

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,7 +1,7 @@
 import type { AstroSharedContext } from "astro";
 
 import { dbService } from "$lib/server/services/database.ts";
-import { catchErrorTyped } from "$lib/utils";
+import { catchErrorTyped } from "$lib/utils/index.ts";
 
 export const prerender = false;
 
@@ -9,18 +9,20 @@ export async function GET({ request, params }: AstroSharedContext) {
   const url = new URL(request.url);
 
   if (url.searchParams.get("database")) {
-    const { error: eDB, data: db } = await catchErrorTyped(dbService.getDB());
+    const { error: eDB } = await catchErrorTyped(dbService.getDB());
     if (eDB) return new Response(null, { status: 500 });
 
-    const { error, data } = await catchErrorTyped(db.query("RETURN 'foo'"));
-    if (error) {
+    if (eDB) {
       return new Response(
         JSON.stringify({ error: "Database error" }),
         { status: 500 },
       );
     }
 
-    return new Response(JSON.stringify(data), { status: 200 });
+    return new Response(
+      JSON.stringify({ message: "Database connection successful" }),
+      { status: 200 },
+    );
   }
 
   return new Response(JSON.stringify({ hello: "world" }), {
